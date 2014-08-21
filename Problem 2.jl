861ddf5f39bc82e3cfe0f81eb56d765b18ad5255
Andrew Christianson <andrew.christianson@gmail.com>
2014-08-20 20:38:09 -0700

    Problem 3 Python - this hammer is a little large

--- Problem 2.jl
+++ Problem 2.jl
@@ -1,3 +1,28 @@
 function fib(max_val)
-	
+	res, val, i_m2 = 0, 0, 0
+	i_m1 = 1
+
+	while val < max_val
+		val = i_m1 + i_m2
+		i_m1, i_m2 = val, i_m1
+		((val & 1) == 0) && (res += val)
+	end 
+	println("The imperative answer to Euler Problem 2 is $res")
 end
+
+@time fib(4000000)
+
+function fibarr(max_val, )
+	arr = [1,1]
+	# println(arr)
+	while arr[end] < max_val
+		arr = [arr, arr[end]+arr[end-1]]
+	end
+	arr
+end
+
+@time (farr = fibarr(4000000);
+       res2 = +(filter(x -> x & 1 == 0, farr)...))
+
+println("The slightly more idiomatic answer to Problem 3 is $res2")
+
---
+++ Problem 3.py
@@ -0,0 +1,62 @@
+from __future__ import print_function, division
+import math
+import sys
+# Borrowed from scipy/lib/six.py (sort of)
+if sys.version_info.major == 2:
+    import __builtin__
+    builtins = __builtin__
+    xrange = __builtin__.xrange
+else:
+    import builtins
+    xrange = builtins.range
+
+class memoize(object):
+
+    def __init__(self, func):
+        self.func = func
+        self.mdict = {}
+
+    def __call__(self, arg):
+        try:
+            return self.mdict[arg]
+        except KeyError:
+            self.mdict[arg] = self.func(arg)
+            return self.mdict[arg]
+
+@memoize
+def isprime(num):
+    bound = math.sqrt(num)
+    if num & 1 == 0:
+        return False
+    for cfactor in xrange(3, int(bound + 1)):
+        if num % cfactor == 0:
+            return False
+    return True
+
+def cfactor(num):
+    inum = num
+    factors = set()
+    while True:
+        for fac in xrange(2, int(math.sqrt(num)) + 1):
+            if num % fac == 0:
+                factors.add(fac)
+                # num = int(num / fac)
+                factors.add(num)
+                continue
+        break
+    # factors.add(int(inum / num))
+    print(sorted(list(factors)))
+    return factors
+    
+if __name__ == '__main__':
+    count = 0
+    factors = set([1])
+    val = 600851475143 
+    while True:
+        for fac in range(max(factors) + 1, int(math.sqrt(val))):
+            if val % fac == 0 and isprime(fac):
+                factors.add(fac)
+                # print(fac)
+        break
+    print("The answer to Euler Problem 3 is {}".format(max(factors)))
+
