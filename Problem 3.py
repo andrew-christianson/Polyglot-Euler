44922ffb9279b302358119a2e2a56f562526d25e
Andrew Christianson <andrew.christianson@gmail.com>
2014-09-01 12:33:42 -0700

    bump

--- Problem 3.py
+++ Problem 3.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import math
 import sys
+
 # Borrowed from scipy/lib/six.py (sort of)
 if sys.version_info.major == 2:
     import __builtin__
@@ -25,6 +26,8 @@ class memoize(object):
 
 @memoize
 def isprime(num):
+    if num == 2:
+        return True
     bound = math.sqrt(num)
     if num & 1 == 0:
         return False
@@ -40,18 +43,16 @@ def cfactor(num):
         for fac in xrange(2, int(math.sqrt(num)) + 1):
             if num % fac == 0:
                 factors.add(fac)
-                # num = int(num / fac)
                 factors.add(num)
                 continue
         break
-    # factors.add(int(inum / num))
     print(sorted(list(factors)))
     return factors
-    
+
 if __name__ == '__main__':
     count = 0
     factors = set([1])
-    val = 600851475143 
+    val = 600851475143
     while True:
         for fac in range(max(factors) + 1, int(math.sqrt(val))):
             if val % fac == 0 and isprime(fac):
@@ -59,4 +60,3 @@ if __name__ == '__main__':
                 # print(fac)
         break
     print("The answer to Euler Problem 3 is {}".format(max(factors)))
-
